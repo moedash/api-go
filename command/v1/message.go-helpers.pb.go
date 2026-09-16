08ee639b9c2be06cac080e18a59d4f638d5e0abe
Mohammad Dashti <mohammad.dashti@temporal.io>
2026-09-16 15:35:58 -0700

    Added the generated Go for the stream commands and events.
    Regenerated from the stream protos, with the submodule pointed at the
    branch that carries them.

--- command/v1/message.go-helpers.pb.go
+++ command/v1/message.go-helpers.pb.go
@@ -670,3 +670,77 @@ func (this *Command) Equal(that interface{}) bool {
 
 	return proto.Equal(this, that1)
 }
+
+// Marshal an object of type AddStreamMessagesCommandAttributes to the protobuf v3 wire format
+func (val *AddStreamMessagesCommandAttributes) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type AddStreamMessagesCommandAttributes from the protobuf v3 wire format
+func (val *AddStreamMessagesCommandAttributes) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *AddStreamMessagesCommandAttributes) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two AddStreamMessagesCommandAttributes values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *AddStreamMessagesCommandAttributes) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *AddStreamMessagesCommandAttributes
+	switch t := that.(type) {
+	case *AddStreamMessagesCommandAttributes:
+		that1 = t
+	case AddStreamMessagesCommandAttributes:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
+// Marshal an object of type SubscribeStreamCommandAttributes to the protobuf v3 wire format
+func (val *SubscribeStreamCommandAttributes) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type SubscribeStreamCommandAttributes from the protobuf v3 wire format
+func (val *SubscribeStreamCommandAttributes) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *SubscribeStreamCommandAttributes) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two SubscribeStreamCommandAttributes values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *SubscribeStreamCommandAttributes) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *SubscribeStreamCommandAttributes
+	switch t := that.(type) {
+	case *SubscribeStreamCommandAttributes:
+		that1 = t
+	case SubscribeStreamCommandAttributes:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
